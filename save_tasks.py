78965e1d4cd50fedffa3e3fa3c33f42f1bf7a636
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-06 23:13:06 +0500

    save tasks

---
+++ save_tasks.py
@@ -0,0 +1,7 @@
+FILENAME = "tasks.txt"
+
+def save_tasks(tasks):
+    with open(FILENAME, "w") as file:
+        for task_id, task in tasks.items():
+            file.write(f"{task_id} | {task["title"]} | {task["status"]}\n")
+            
