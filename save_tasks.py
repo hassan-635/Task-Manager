712ca7bd899084b13975e1e78a8d52d2dc4a408a
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-07 07:10:25 +0500

    debug: fstring mismatch fixed

--- save_tasks.py
+++ save_tasks.py
@@ -3,5 +3,5 @@ FILENAME = "tasks.txt"
 def save_tasks(tasks):
     with open(FILENAME, "w") as file:
         for task_id, task in tasks.items():
-            file.write(f"{task_id} | {task["title"]} | {task["status"]}\n")
+            file.write(f"{task_id} | {task['title']} | {task['status']}\n")
             
