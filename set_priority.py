63d5cab47a7288540e94f5a01ae567a96f53d6d1
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-07 07:55:00 +0500

    task priority

--- set_priority.py
+++ set_priority.py
@@ -1,8 +1,9 @@
 
 
-def set_priority(tasks, priority):
+def set_priority(tasks):
     task_id = int(input("Please Enter Task Id : "))
     if task_id in tasks:
+        priority = input("Please Enter priority (H/L) : ")
         tasks[task_id]["priority"] = priority
         print("Task Priority set...")
     else:
