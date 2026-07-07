6354f8da9f6a341c7429c722c5f21aa746072e20
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-07 10:27:35 +0500

    debugged

--- save_tasks.py
+++ save_tasks.py
@@ -3,5 +3,4 @@ FILENAME = "tasks.txt"
 def save_tasks(tasks):
     with open(FILENAME, "w") as file:
         for task_id, task in tasks.items():
-            file.write(f"{task_id} | {task['title']} | {task['status'] | task['priority']}\n")
-            
+            file.write(f"{task_id} | {task['title']} | {task['status']} | {task.get('priority', 'N/A')}\n")
--- set_priority.py
+++ set_priority.py
@@ -1,10 +1,12 @@
 
 
 def set_priority(tasks):
-    task_id = int(input("Please Enter Task Id : "))
+    task_id = input("Please Enter Task Id : ")
+    task_id = int(task_id)
     if task_id in tasks:
         priority = input("Please Enter priority (H/L) : ")
         tasks[task_id]["priority"] = priority
         print("Task Priority set...")
+
     else:
         print("No Task Available ...")
