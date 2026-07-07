ff346ce378d2c1310ebfc2ff7e1ffef8a7a92d78
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-07 07:09:32 +0500

    debug: fstring mismatch fixed

--- delete_task.py
+++ delete_task.py
@@ -4,6 +4,6 @@ def delete_task(tasks):
     task_id = int(input("Please Enter Task ID : "))
     if task_id in tasks:
         deleted_tasks = tasks.pop(task_id)
-        print(f"Task {deleted_tasks["title"]} Deleted...")
+        print(f"Task {deleted_tasks['title']} Deleted...")
     else:
-        print(f"TAsk {task_id} Not Found...")
+        print(f"Task {task_id} Not Found...")
