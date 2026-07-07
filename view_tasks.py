fe1d58a7fffd1abe505e935f301106d872655d43
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-07-07 07:37:02 +0500

    test

--- view_tasks.py
+++ view_tasks.py
@@ -3,5 +3,5 @@ def view_tasks(tasks):
     if not tasks:
         print("No task Available...")
     else:
-        for task_id, task in tasks:
+        for task_id, task in tasks.items():
             print(f"[{task_id}] -> {task['title']} -> {task['status']}")
